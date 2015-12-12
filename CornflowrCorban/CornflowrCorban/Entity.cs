2be8b29e5d03e4810d07e996b51ac8864f55bfc9
F1tZy <f1tz81@gmail.com>
2015-12-11 22:10:49 -0500

    added entity and player classes

---
+++ CornflowrCorban/CornflowrCorban/Entity.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CornflowrCorban
+{
+    public class Entity
+    {
+        public Texture2D Image { get; set; }
+        public float Scale { get; set; }
+        public Vector2 Position { get; set; }
+
+        public Rectangle HitBox { get; set; }
+
+        public List<Point> AIPath { get; set; }
+
+    }
+}
---
+++ CornflowrCorban/CornflowrCorban/WhaleOfAPlayer.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CornflowrCorban
+{
+    public class WhaleOfAPlayer : Entity
+    {
+        public WhaleOfAPlayer(Texture2D image)
+        {
+            Image = image;
+            this.Scale = 1;
+            this.Position = new Microsoft.Xna.Framework.Vector2(10, 10);
+            this.HitBox = new Microsoft.Xna.Framework.Rectangle((int)Position.X, (int)Position.Y, Image.Width, Image.Height);
+        }
+    }
+}
