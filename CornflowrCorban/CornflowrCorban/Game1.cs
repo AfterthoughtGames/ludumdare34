281dedb0be7e781a362e71c0c0e2119b249d8df5
F1tZy <f1tz81@gmail.com>
2015-12-13 00:07:34 -0500

    fixed title and score retetion

--- CornflowrCorban/CornflowrCorban/Game1.cs
+++ CornflowrCorban/CornflowrCorban/Game1.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace CornflowrCorban
 {
@@ -55,6 +56,7 @@ namespace CornflowrCorban
 
         public static bool InMenu = true;
         public static bool StartNewGame = false;
+        public static bool ExitGame = false;
 
         SoundEffect se;
 
@@ -111,7 +113,7 @@ namespace CornflowrCorban
             Pixel = new Texture2D(spriteBatch.GraphicsDevice, 1, 1);
             Pixel.SetData<Color>(new Color[] { Color.White });
 
-            TitleScreen = new Title(Content.Load<Texture2D>("Title"));
+            TitleScreen = new Title(Content.Load<Texture2D>("Title"), GUIFont);
             lasers = new List<Laser>();
             bubbles = new List<Bubble>();
             topBubbles = new List<Bubble>();
@@ -141,6 +143,8 @@ namespace CornflowrCorban
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
+            if (ExitGame) Exit();
+
             if (InMenu)
             {
                 TitleScreen.Update(gameTime);
@@ -163,7 +167,7 @@ namespace CornflowrCorban
                 KeyboardState newState = Keyboard.GetState();
 
                 if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                    Exit();
+                    InMenu = true;//Exit();
 
                 if (newState.IsKeyDown(Keys.W) || GamePad.GetState(PlayerIndex.One).ThumbSticks.Left.Y > 0)
                 {
@@ -238,7 +242,11 @@ namespace CornflowrCorban
 
                 currentTime = gameTime.TotalGameTime;
 
-                if (Player.Dead) InMenu = true;
+                if (Player.Dead)
+                {
+                    SaveScore();
+                    InMenu = true;
+                }
             }
 
 
@@ -435,6 +443,7 @@ namespace CornflowrCorban
 
         private void NewGame(GameTime gameTime)
         {
+            Score = 0;
             Player = new WhaleOfAPlayer(Content.Load<Texture2D>("Whale"));
 
             lasers = new List<Laser>();
@@ -449,6 +458,31 @@ namespace CornflowrCorban
                 new SimpleBadFish(Content.Load<Texture2D>("shark_0000_3"), Vector2.Zero, new Vector2(-100, 0), 1));
 
             StartNewGame = false;
+        }
+
+        private void SaveScore()
+        {
+            if (File.Exists(Directory.GetCurrentDirectory() + "\\score.txt")) File.Delete(Directory.GetCurrentDirectory() + "\\score.txt");
+            System.IO.File.WriteAllText(Directory.GetCurrentDirectory() + "\\score.txt", Score.ToString(), System.Text.Encoding.ASCII);
+        }
+
+        public static int ReadScore()
+        {
+            if (File.Exists(Directory.GetCurrentDirectory() + "\\score.txt"))
+            {
+                String line = string.Empty;
+
+                using (StreamReader sr = new StreamReader(Directory.GetCurrentDirectory() + "\\score.txt"))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    line = sr.ReadToEnd();
+                    Console.WriteLine(line);
+                }
+
+                return Convert.ToInt32(line);
+            }
+
+            return 0;
         }
     }
 }
--- CornflowrCorban/CornflowrCorban/Title.cs
+++ CornflowrCorban/CornflowrCorban/Title.cs
@@ -11,24 +11,32 @@ namespace CornflowrCorban
     public class Title
     {
         Texture2D titleImage { get; set; }
+        SpriteFont guiText { get; set; }
 
-        public Title(Texture2D image)
+        public Title(Texture2D image, SpriteFont gui)
         {
             titleImage = image;
+            guiText = gui;
         }
 
         public void Update(GameTime gameTime)
         {
-            if (Keyboard.GetState().IsKeyDown(Keys.Space) || GamePad.GetState(PlayerIndex.One).Buttons.A == ButtonState.Pressed)
+            if (Keyboard.GetState().IsKeyDown(Keys.Enter) || GamePad.GetState(PlayerIndex.One).Buttons.Start == ButtonState.Pressed)
             {
                 Game1.StartNewGame = true;
                 Game1.InMenu = false;
             }
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Game1.ExitGame = true;//Exit();
         }
 
         public void Draw(GameTime gameTime, SpriteBatch sb)
         {
             sb.Draw(titleImage, Vector2.Zero, Color.White);
+            sb.DrawString(guiText, "Previous Score: " + Game1.ReadScore().ToString(), new Vector2(sb.GraphicsDevice.Viewport.Width - 160, 10), Color.Black);
+            sb.DrawString(guiText, "Press Enter or Start Button to Play", new Vector2((sb.GraphicsDevice.Viewport.Width/ 2) - 100, sb.GraphicsDevice.Viewport.Height -250), Color.Red);
+            sb.DrawString(guiText, "Press Esc or Back Button to Exit", new Vector2((sb.GraphicsDevice.Viewport.Width / 2) - 88, sb.GraphicsDevice.Viewport.Height - 224), Color.Red);
         }
     }
 }
